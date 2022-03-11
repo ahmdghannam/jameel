5c29eaf648e654324bbc38292847cb116358ccda
iia7mt <a.ghannam18@student.aaup.edu>
2022-03-11 09:54:58 +0200

    last added

--- src/bat/aaup/Main.java
+++ src/bat/aaup/Main.java
@@ -1,32 +1,45 @@
 package bat.aaup;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
+//        int max;
+//        max = Integer.MIN_VALUE;
+//
+//        int []a= {1,6,8,0,8};
+//
+//        for (int i = 1; i < a.length; i++) {
+//            if (a[i]>max)max=a[i];
+//        }
+//        for (int i = 0; i < 100; i++) {
+//            System.out.println();
+//
+//        }
+//        System.out.println(max);
+//        Batata batata=new Batata();
+        int a = 0;
+//        ArrayList<Integer> arr = new ArrayList<>();
+//        arr.add(9);
+//        arr.add(7);
 
-        int max;
-        max = Integer.MIN_VALUE;
-
-        int []a= {1,6,8,0,8};
-
-        for (int i = 1; i < a.length; i++) {
-            if (a[i]>max)max=a[i];
+        int i;
+        for (i = 0; i < 5; i++) {
+            System.out.println(i);
         }
-        for (int i = 0; i < 100; i++) {
-            System.out.println();
 
-        }
-        System.out.println(max);
-        Batata batata=new Batata();
-
-
+        System.out.println("before");
+        System.out.println("after");
 
 
     }
-    public static void haha(){
+
+    public static void haha() {
 
     }
+
     private static class Batata {
 
     }
